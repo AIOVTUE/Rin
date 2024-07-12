f4294a52bb8c0114313cc3e0898f27b33b2f66d8
AIOVTUE <146168353+AIOVTUE@users.noreply.github.com>
2024-07-12 15:04:42 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -46,9 +46,9 @@ function Footer() {
                 {footerHtml && <div dangerouslySetInnerHTML={{ __html: footerHtml }} />}
                 <p className='text-sm text-neutral-500 font-normal link-line'>
                     <span>
-                        AIOVTUE<br>
-                        <a href="https://icp.gov.moe/?keyword=20245002" target="_blank">萌ICP备20245002号</a ><br>
-                        Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        <p>AIOVTUE</p>
+                        <p><a href="https://icp.gov.moe/?keyword=20245002" target="_blank">萌ICP备20245002号</a ></p>
+                        <p>Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a></p>
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
